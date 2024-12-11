2e0e9595770bd7713efafce2449e4004a3018a3d
Isaac Ballone <isaac@ballone.dev>
2024-12-11 13:43:55 -0600

    Add Nix flake

---
+++ flake.nix
@@ -0,0 +1,27 @@
+{
+  description = "cosing";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-24.11";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = { self, nixpkgs, flake-utils }:
+    let system = flake-utils.lib.system;
+    in flake-utils.lib.eachSystem [
+      system.x86_64-linux
+      system.aarch64-linux
+      system.aarch64-darwin
+    ] (system:
+      let pkgs = nixpkgs.legacyPackages.${system};
+      in with pkgs; {
+        packages = { inherit source; };
+        devShells.default = mkShell {
+          buildInputs = [
+            clojure
+            jdk21
+            uv
+          ];
+        };
+      });
+}
